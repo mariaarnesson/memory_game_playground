3b2f6347a16d125b1a6db29549d090fae8f68ea0
mariaarnesson <maria_arnesson@outlook.com>
2023-02-07 06:20:33 +0000

    Add function moves

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 const cards = document.querySelectorAll('.memory-card');
+
+const moveContainer = document.querySelector(".moves");
+const instructions = document.getElementById('instructions');
+const timeContainer = document.querySelector(".timer");
+const MAX_MATCH = 6;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let moves = 0;
+let finalTime = "";
+let timeout;
 
+function greet() {
+	alert('Welcome to the memory game!');
+   }
+   setTimeout(greet, 2000); //execute greet after 2000 milliseconds (2 seconds)
 
 function flipCard() {
 	if (lockBoard) return;
@@ -25,7 +37,10 @@ function flipCard() {
 	let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
  	isMatch ? disableCards() : unflipCards();
   }
- 
+  
+  function myFunction() {
+	timeout = setTimeout(alertFunc, 3000);
+  }
   function disableCards() {
 	firstCard.removeEventListener('click', flipCard);
 	secondCard.removeEventListener('click', flipCard);
@@ -36,12 +51,27 @@ function flipCard() {
   function unflipCards() {
 	lockBoard = true;
 	setTimeout(() => {
-	  firstCard.classList.remove('flip');
-	  secondCard.classList.remove('flip');
+        firstCard.classList.add("shake");
+        secondCard.classList.add("shake");
+    }, 400);
+	setTimeout(() => {
+	  firstCard.classList.remove("shake", "flip");
+	  secondCard.classList.remove("shake", "flip");
 	  resetBoard();
 	}, 1500);
 
-  }
+  
+   // Add move
+   addMove();
+}
+ //Move counter
+ moves = 0;
+ moveContainer.innerHtml = 0;
+ 
+ function addMove() {
+     moves++;
+     moveContainer.innerHTML = moves;
+ }
 
   function resetBoard() {
 	[hasFlippedCard, lockBoard] = [false, false];
@@ -56,3 +86,4 @@ function flipCard() {
   })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
