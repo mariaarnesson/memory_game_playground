903c0ac291d6791d3bbc6217bdf4cda92d236f42
mariaarnesson <maria_arnesson@outlook.com>
2023-02-07 12:18:07 +0000

    Initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,90 @@
 const cards = document.querySelectorAll('.memory-card');
-
+const timeContainer = document.querySelector(".timer");
 const moveContainer = document.querySelector(".moves");
 const instructions = document.getElementById('instructions');
-const timeContainer = document.querySelector(".timer");
 const MAX_MATCH = 6;
+const modalBtn = document.getElementById("modalBtn");
+const closeBtn = document.getElementById("closeBtn");
+
+let gameOn = false;
+let perfectMatch = 0;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 let moves = 0;
 let finalTime = "";
-let timeout;
+
+
+
 
 function greet() {
 	alert('Welcome to the memory game!');
    }
    setTimeout(greet, 2000); //execute greet after 2000 milliseconds (2 seconds)
+ //events
+ cards.forEach(card => card.addEventListener('click', flipCard)); // listen for card flips
+ shuffle();
+ 
+ modalBtn.addEventListener('click', showInstructions); // listen for open click of how to play instructions modal
+ closeBtn.addEventListener('click', closeInstructions); // listen for close instructions button
+ 
+ 
+ function showInstructions() {
+     instructions.style.display = "block";
+ }
+ 
+ function closeInstructions() {
+     instructions.style.display = "none";
+ }
+ 
+ /*
+ onclick function for cards, add flip class for css effects
+ code taken form https://marina-ferreira.github.io/tutorials/js/memory-game/ and adapted  
+ */
 
 function flipCard() {
-	if (lockBoard) return;
-	if (this === firstCard) return;
-	this.classList.add('flip');
-
-	if (!hasFlippedCard) {
-	  hasFlippedCard = true;
-	  firstCard = this;
-	  return;
+	if (!gameOn) {
+		gameOn = true;
+		timer();
 	}
- 
-	secondCard = this;
-	
- 
-	checkForMatch();
+	if (lockBoard) return; // return is lockBoard is true so the rest of the function wont be executed
+	if (this === firstCard) return;
+
+	this.classList.add('flip'); //if valid, flips card using css class
+
+	if (!flippedCard) { // The first card clicked
+
+		flippedCard = true;
+		firstCard = this; //stores this as first card
+
+		return;
+
+	}
+
+	secondCard = this; //The second card clicked
+
+	checkCardMatch();
   }
  
-  function checkForMatch() {
-	let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
- 	isMatch ? disableCards() : unflipCards();
-  }
-  
-  function myFunction() {
-	timeout = setTimeout(alertFunc, 3000);
-  }
-  function disableCards() {
+  function checkCardMatch() {
+	let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+	if (isMatch) perfectMatch += 1;
+
+	if (isMatch) pairMatch();
+	else noMatch();
+
+	if (perfectMatch === MAX_MATCH) winGame();
+}
+
+// matched cards will be disabled for clicks once they are flipped
+function pairMatch() {
 	firstCard.removeEventListener('click', flipCard);
 	secondCard.removeEventListener('click', flipCard);
 
 	resetBoard();
   }
  
-  function unflipCards() {
+  function noMatch() {
 	lockBoard = true;
 	setTimeout(() => {
         firstCard.classList.add("shake");
@@ -72,18 +108,84 @@ function flipCard() {
      moves++;
      moveContainer.innerHTML = moves;
  }
+ //timer
+ let time;
+ let minutes = 0;
+ let seconds = 0;
+ let timeStart = false;
+ timeContainer.innerHTML = "Time " + minutes + " : " + seconds;
+ 
+ function timer() {
+     time = setInterval(function() {
+         seconds++;
+         if (seconds === 59) {
+             minutes++;
+             seconds = 0;
+         }
+         timeContainer.innerHTML = "Time " + minutes + " : " + seconds;
+     }, 1000);
+ }
+ 
+ function stopTime() {
+     clearInterval(time);
+ }
+ 
+ //cards are reset after each round
+ 
 
   function resetBoard() {
 	[hasFlippedCard, lockBoard] = [false, false];
 	[firstCard, secondCard] = [null, null];
   }
+ 
+  function winGame() {
+	stopTime();
+	showWinMessage();
+}
 
-  (function shuffle() {
+
+// Win message pop up 
+function showWinMessage() {
+	modal.style.display = "block";
+	finalTime = timeContainer.innerHTML;
+	// showing moves and time on modal
+	document.getElementById("finalMove").innerHTML = moves;
+	document.getElementById("totalTime").innerHTML = finalTime;
+	reset();
+
+
+}
+// when the user clicks the (x) To close modal
+window.onclick = function(event) {
+	if (event.target.id == 'close') {
+		document.getElementById('modal').style.display = "none";
+	}
+};
+
+  function shuffle() {
 	cards.forEach(card => {
 	  let ramdomPos = Math.floor(Math.random() * 12);
 	  card.style.order = ramdomPos;
 	});
-  })();
+}
+ 
+// New Game Button 
+  function reset() {
+	setTimeout(() => {
+		flippedCard = false;
+		[firstCard, secondCard] = [null, null];
+		stopTime();
+		gameOn = false;
+		timeStart = false;
+		seconds = 0;
+		minutes = 0;
+		timeContainer.innerHTML = "Timer 0:00";
+		moves = 0;
+		moveContainer.innerHTML = 0;
+		perfectMatch = 0;
+		cards.forEach(cardReset => cardReset.classList.remove('flip'));
+		shuffle();
+		cards.forEach(card => card.addEventListener('click', flipCard));
+	}, 500);
 
-cards.forEach(card => card.addEventListener('click', flipCard));
-
+}
